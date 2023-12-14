ac82f1f51df2aebe1bb4910ebd1f774ff1e2be5b
Eliza Coroiu <eliza.coroiu@essensys.ro>
2023-12-14 12:34:27 +0200

    added OnDestroy and implemented it in HeaderComponent

--- src/app/modules/core/components/header/header.component.ts
+++ src/app/modules/core/components/header/header.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CountryService } from '../../../standings/services/country.service';
 import { Country } from '../../../standings/models/Country';
 import { FootballService } from '../../services/football.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   countries: Country[] = [];
-
+  countrySubscription: Subscription = new Subscription();
   constructor(private apiService: FootballService, private countryService: CountryService) { }
 
   ngOnInit(): void {
@@ -18,14 +19,20 @@ export class HeaderComponent implements OnInit {
   }
 
   getCountries(): Country[] {
-    this.countryService.getCountries().subscribe((response) => {
+    this.countrySubscription = this.countryService.getCountries().subscribe((response) => {
       this.countries = response;
     });
-    
+
     return this.countries;
   }
 
   displayStandings(country: Country) {
     this.apiService.standingChangeNotification.next(country);
+  }
+
+  ngOnDestroy(): void {
+    if (this.countrySubscription) {
+      this.countrySubscription.unsubscribe();
+    }
   }
 }
